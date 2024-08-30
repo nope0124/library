4de0d40dccfe5cc6409ae8520982d38b8fe841d6
nope0124 <kami-ryo0124@ezweb.ne.jp>
2024-08-31 00:59:27 +0900

    トポロジカルソート

---
+++ graph/topological_sort.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <vector>
+#include <queue>
+#include "graph_template.hpp"
+
+template<class T> std::vector<int> topological_sort(Graph<T> &graph) {
+    int n = (int)(graph.size());
+    std::vector<int> res, indeg(n, 0);
+
+    for (int i = 0; i < n; i++) {
+        for (auto c: graph[i]) {
+            indeg[c]++;
+        }
+    }
+
+    std::queue<int> que;
+    for (int i = 0; i < n; i++) {
+        if (indeg[i] == 0) que.push(i);
+    }
+
+    while (!que.empty()) {
+        auto v = que.front(); que.pop();
+        res.push_back(v);
+        for (auto c: graph[v]) {
+            indeg[c]--;
+            assert(indeg[c] >= 0);
+            if (indeg[c] == 0) que.push(c);
+        }
+    }
+
+    return res;
+}
