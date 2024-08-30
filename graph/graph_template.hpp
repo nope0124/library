4df77e699d84d26f1490ea7cfb9fac4fa052ca6f
nope0124 <kami-ryo0124@ezweb.ne.jp>
2024-08-30 23:10:09 +0900

    グラフテンプレート修正

--- graph/graph_template.hpp
+++ graph/graph_template.hpp
@@ -3,9 +3,35 @@
 #include <vector>
 
 template<class T> struct Graph {
+    int n;
     std::vector<std::vector<T>> graph;
 
-    Graph(int n) : graph(n, std::vector<T>()) {}
+    Graph(int n) : n(n), graph(n, std::vector<T>()) {}
     inline std::vector<T>& operator[] (int i) { return graph[i]; }
     size_t size() const { return graph.size(); }
+
+    std::vector<int> preorder, postorder;
+
+    /**
+     * @brief 行きがけ順、帰りがけ順の配列を作る
+     */
+    void calculateOrder() {
+        std::vector<bool> reached(n, false);
+        auto dfs = [&](auto f, int v, int p) -> void {
+            if (reached[v]) return;
+            reached[v] = true;
+            preorder.push_back(v);
+            for (auto c: graph[v]) {
+                if (c == p) continue;
+                f(f, c, v);
+            }
+            postorder.push_back(v);
+            return;
+        };
+
+        for (int i = 0; i < n; i++) if (!reached[i]) dfs(dfs, i, -1);
+
+        assert((int)(preorder.size()) == n);
+        assert((int)(postorder.size()) == n);
+    }
 };
