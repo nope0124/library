cd49f244bdb9450ef4934c42f51e8db6efbfa4a7
nope0124 <kami-ryo0124@ezweb.ne.jp>
2024-09-01 05:35:40 +0900

    二部グラフ

--- verify/graph/bipartite_graph_1.test.cpp
+++ verify/graph/bipartite_graph_1.test.cpp
@@ -1,4 +1,4 @@
-#define PROBLEM "https://atcoder.jp/contests/math-and-algorithm/tasks/math_and_algorithm_ao"
+#define PROBLEM "https://atcoder.jp/contests/abc327/tasks/abc327_d"
 #include <bits/stdc++.h>
 #include "graph/graph_template.hpp"
 #include "graph/bipartite_graph.hpp"
@@ -8,11 +8,13 @@ typedef long long int ll;
 
 int main() {
     ll N, M; cin >> N >> M;
+    vector<ll> A(M), B(M);
     Graph<ll> graph(N);
+    rep (i, M) cin >> A[i], A[i]--;
+    rep (i, M) cin >> B[i], B[i]--;
     rep (i, M) {
-        ll a, b; cin >> a >> b; a--; b--;
-        graph[a].push_back(b);
-        graph[b].push_back(a);
+        graph[A[i]].push_back(B[i]);
+        graph[B[i]].push_back(A[i]);
     }
 
     BipartiteGraph<ll> bg(graph);
